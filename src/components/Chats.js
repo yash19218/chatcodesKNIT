da0f00f6f3bc08714d6c91b30a89fa4290daf827
Ayushman <Asus@DESKTOP-3RK8C8O.(none)>
2022-04-19 18:03:14 +0530

    improve loading state using react-spinners

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -2,6 +2,7 @@ import React , {useRef,useState,useEffect }from 'react';
 import { useHistory } from 'react-router-dom';
 import {  ChatEngine } from 'react-chat-engine';
 import { authentication } from '../firebase';
+import { HashLoader } from 'react-spinners';
 
 import { useAuth } from '../contexts/AuthContext';
 import axios from 'axios';//for making diffrent api's call!
@@ -59,7 +60,11 @@ const Chats = () =>{
 
     },[user,history])
 
-    if(!user || loading) return 'Loading...';
+    if(!user || loading) return(
+        <div className="Loading">
+            <HashLoader size={80}  color="blue"/>
+        </div>  
+    );
 
     return (
         
